5525e43a46d5c355a5a7cebf47b2cd446ed94a37
harsh2g <harshgadani55@gmail.com>
2017-11-04 01:56:13 -0400

    Deleted Ionic

--- test001/src/pages/signup/signup.ts
+++ test001/src/pages/signup/signup.ts
@@ -14,10 +14,13 @@ export class SignupPage {
   // The account fields for the login form.
   // If you're using the username field with or without email, make
   // sure to add it to the type
-  account: { name: string, email: string, password: string } = {
+  
+  account: { name: string, email: string, password: string, phonenumber: string, household: int } = {
     name: 'Test Human',
     email: 'test@example.com',
-    password: 'test'
+    password: 'test',
+	phonenumber: '555-555-5555',
+	household: 2
   };
 
   // Our translated text strings
